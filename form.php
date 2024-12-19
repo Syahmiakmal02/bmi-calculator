2c1ea1347655e9aaccb217f438f386e069fbf75d
kemallio <D20221101856@siswa.upsi.edu.my>
2024-12-19 12:41:58 +0800

    adjusted welcome

--- form.php
+++ form.php
@@ -4,8 +4,13 @@
 
     <form action="welcome.php" method="POST">
         Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
+        Height: <input type="number" name="height"><br>
+        Weight: <input type="number" name="weight"><br>
+        Gender: <br>
+        Male: <input type="radio" name="gender" id="gender_male" value="male" >
+        Female: <input type="radio" name="gender" id="gender_female" value="female" >
+
+        <input type="submit" value="Submit">
     </form>
 
 </body>
--- welcome.php
+++ welcome.php
@@ -1,10 +1,30 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI Result</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
+    <?php
+    $name = $_POST["name"] ?? '';
+    $height = $_POST["height"] ?? 0;
+    $weight = $_POST["weight"] ?? 0;
+    $gender = $_POST["gender"] ?? '';
 
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
+    // Calculate BMI
+    $heightInMeters = $height / 100;
+    $bmi = $weight / ($heightInMeters * $heightInMeters);
+    $bmi = round($bmi, 2);
+    ?>
+
+    <h2>BMI Results</h2>
+    <p>Name: <?php echo htmlspecialchars($name); ?></p>
+    <p>Height: <?php echo htmlspecialchars($height); ?> cm</p>
+    <p>Weight: <?php echo htmlspecialchars($weight); ?> kg</p>
+    <p>Gender: <?php echo htmlspecialchars($gender); ?></p>
+    <p>Your BMI is: <?php echo $bmi; ?></p>
 
 </body>
-
 </html>
